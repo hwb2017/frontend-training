3756aaf3feb6b147017ecd01c83e7568f60e826d
hwb2017 <1003673802@qq.com>
2022-04-03 21:33:04 +0800

    增加react实战网易云音乐项目

---
+++ react/cloud-music/src/App.js
@@ -0,0 +1,14 @@
+import { GlobalStyle } from './style';
+import { IconStyle } from './assets/iconfont/iconfont';
+
+function App() {
+  return (
+    <div className="App">
+      <GlobalStyle/>
+      <IconStyle/>
+      <i className='iconfont'>&#xe62b;</i>
+    </div>
+  );
+}
+
+export default App;
