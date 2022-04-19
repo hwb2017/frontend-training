683d7b3e1f3ef870a4af111393799db3ba238845
hwb2017 <1003673802@qq.com>
2022-04-20 07:09:24 +0800

    修复scroll组件无法滚动的问题

--- react/cloud-music/src/components/scroll/style.ts
+++ react/cloud-music/src/components/scroll/style.ts
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 const ScrollContainer = styled.div`
   width: 100%;
   height: 100%;
-  overflow: hidden;
+  overflow: scroll;
 `
 
 export default ScrollContainer;
