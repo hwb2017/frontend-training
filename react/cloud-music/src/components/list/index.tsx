29767c0f87adb4e229d6fd64b10309404bebc055
huangwenbin01 <huangwenbin01@bytedance.com>
2022-04-24 13:19:01 +0800

    增加图像懒加载功能

--- react/cloud-music/src/components/list/index.tsx
+++ react/cloud-music/src/components/list/index.tsx
@@ -25,7 +25,7 @@ const RecommendList = (props: ListProps) => {
                 <div className='img_wrapper'>
                   <div className='decorate'></div>
                   {/* 加此参数可以减小请求的图片资源大小, 由图床支持 */}
-                  <img src={item.picUrl + "?param=300x300"} width="100%" height="100%" alt="music" />
+                  <img src={item.picUrl + "?param=300x300"} width="100%" height="100%" alt="music" loading="lazy" />
                   <div className='play_count'>
                     <i className='iconfont paly'>&#xe885;</i>
                     <span className='count'>{formatCount(item.playCount)}</span>
